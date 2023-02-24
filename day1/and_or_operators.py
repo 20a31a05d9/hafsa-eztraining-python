2dce43ed8fcd3f677949d3c79099c959f0404c7d
20a31a05d9 <109792827+20a31a05d9@users.noreply.github.com>
2023-02-24 11:31:27 +0530

    Add files via upload

---
+++ day1/and_or_operators.py
@@ -0,0 +1,10 @@
+a=1<10 and 33>10
+b=1<10 and 33<10
+c=1>0 and 2>1
+d=2<4 or 2>4
+e=2<1 or 1<6
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
---
+++ day1/dtypes.py
@@ -0,0 +1,20 @@
+num1=int("enter 1st integer:") # int(input()) vadali 
+num2=int("enter 2nd integer:")  # just input() annadi string la teeskuntadi input ni 
+num3=int("enter 3rd integer:")
+f1=float("enter 1st float:")
+f2=float("enter 2nd float:")
+f3=float("enter 3rd float:")
+s1=input("enter 1st string:")
+s2=input("enter 2nd string:")
+c1=complex(input("enter 1st complex number:"))
+           #output
+print("1st integer:" +num1)         # + annadi string ney chyagaladu 
+print("2nd integer:" +num2)
+print("3rd integer:" +num3)        # , vadali
+print("1st float:" +f1)
+print("2nd float:" +f2)
+print("3rd float:" +f3)
+print("1st string:"+s1)
+print("2nd string:"+s2)
+print("1st complex number:"+c1)
+ 
